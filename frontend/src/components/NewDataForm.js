ee86d7a7d87c3a3d1b10233dc82d4a328005be67
Jakub.Krutowski <kuba.krutowski@gmail.com>
2023-11-13 07:36:44 -0500

    Final

--- frontend/src/components/NewDataForm.js
+++ frontend/src/components/NewDataForm.js
@@ -1,44 +1,45 @@
 import React from "react";
 import { Button, Form, FormGroup, Input, Label } from "reactstrap";
-
 import axios from "axios";
-
 import { API_URL } from "../constants";
 
 class NewDataForm extends React.Component {
   state = {
-    pk: 0,
-    data: ""
+    data1: "",
+    data2: ""
   };
 
   componentDidMount() {
     if (this.props.data) {
-      const { pk, data } = this.props.data;
-      this.setState({ pk, data });
+      const { pk, data1, data2 } = this.props.data;
+      this.setState({ pk, data1, data2 });
     }
   }
 
-  onChange = e => {
+  onChange = (e) => {
     this.setState({ [e.target.name]: e.target.value });
   };
 
-  createData = e => {
+  createData = (e) => {
+    const data = { "data1": this.state.data1, "data2": this.state.data2 };
     e.preventDefault();
-    axios.post(API_URL, this.state).then(() => {
+    axios.post(API_URL, data).then(() => {
       this.props.resetState();
       this.props.toggle();
     });
+    console.log("test");
+  
   };
-
-  editData = e => {
+  editData = (e) => {
     e.preventDefault();
     axios.put(API_URL + this.state.pk, this.state).then(() => {
       this.props.resetState();
       this.props.toggle();
     });
+    console.log("test1");
   };
 
-  defaultIfEmpty = value => {
+  defaultIfEmpty = (value) => {
     return value === "" ? "" : value;
   };
 
@@ -46,20 +47,22 @@ class NewDataForm extends React.Component {
     return (
       <Form onSubmit={this.props.data ? this.editData : this.createData}>
         <FormGroup>
-          <Label for="data">Data 1:</Label>
-          <Input
-            type="text1"
-            name="data1"
-            onChange={this.onChange}
-            value={this.defaultIfEmpty(this.state.data)}
-          />
-          <Label for="data">Data 2:</Label>
-          <Input
-            type="text2"
-            name="data2"
-            onChange={this.onChange}
-            value={this.defaultIfEmpty(this.state.data)}
-          />
+        <Label for="data1">Data 1:</Label>
+  <Input
+    type="text"
+    name="data1"
+    onChange={this.onChange}
+    value={this.defaultIfEmpty(this.state.data1)}
+  />
+        </FormGroup>
+        <FormGroup>
+        <Label for="data2">Data 2:</Label>
+  <Input
+    type="text"
+    name="data2"
+    onChange={this.onChange}
+    value={this.defaultIfEmpty(this.state.data2)}
+  />
         </FormGroup>
         <Button>Send</Button>
       </Form>
